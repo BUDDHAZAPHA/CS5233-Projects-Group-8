82c501b9c82c63e880f349fd59eb4822b1a7de2a
mjethra <monicajethra@gmail.com>
2022-02-28 13:54:00 +0800

    update gitignore

--- O2DESNet.Libraries/O2DESNet/FutureEventList.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace O2DESNet
-{
-    public class FutureEventList : SortedSet<Event>
-    {
-        public FutureEventList() : base(EventComparer.Instance) { }
-    }
-}
