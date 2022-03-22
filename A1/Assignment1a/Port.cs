1d108e7f9638c5e0925a4d4773e63748f396c8de
Mark Myers <mbmyersii@u.nus.edu>
2022-03-22 16:10:51 +0800

    Repo Organization

---
+++ A1/Assignment1a/Port.cs
@@ -0,0 +1,166 @@
+﻿using O2DESNet;
+using MathNet;
+using O2DESNet.Distributions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1a
+{
+   public class PortStats
+    {
+        public double ExpectedArrivalTime { get; set; }
+        public double ExpectedServiceStartTime { get; set; }
+        public double ExpectedServiceEndTime { get; set; }
+        public int ServerCapacity { get; set; }
+
+    }
+    public class Port : Sandbox
+    {
+
+        public int NoInServer1 { get; set; }
+        public int NoInServer2 { get; set; }
+        public int NoInQueue { get; set; }
+        public List<Ship> PendingList = new();
+        public List<Ship> ProcessedList = new();
+        public int ProcessCounts
+        {
+            get
+            {
+                 var shipLists= ProcessedList.Select(ship => (ship.TimeStamp_End - ship.TimeStamp_Arrive).TotalDays).ToList();
+                 return shipLists.Where(x => x > 2.0).Select(x=>x).Count();    
+                 
+            }
+        }
+
+        public double AverageCycleTime
+        {
+            get
+            {
+                return ProcessedList.Average(ship => (ship.TimeStamp_End - ship.TimeStamp_Arrive).TotalDays);
+            }
+        }
+
+        public double MaxBerthTime
+        {
+            get
+            {
+                return ProcessedList.Max(ship => (ship.TimeStamp_End - ship.TimeStamp_Arrive).TotalDays);
+            }
+        }
+        public double MinBerthTime
+        {
+            get
+            {
+                return ProcessedList.Min(ship => (ship.TimeStamp_End - ship.TimeStamp_Arrive).TotalDays);
+            }
+        }
+
+        public double AverageWaitingTime
+        {
+            get
+            {
+                return ProcessedList.Average(ship => (ship.TimeStamp_End - ship.TimeStamp_Start).TotalDays);
+            }
+        }
+        public HourCounter HC_InQueue { get; set; }
+        public HourCounter HC_InServer1 { get; set; }
+        public HourCounter HC_InServer2 { get; set; }
+        public HourCounter HC_InSystem { get; set; }
+        public PortStats Config { get; set; }
+
+        
+        public Port(PortStats config,int seed) : base(seed)
+        {
+            Config = config;
+            HC_InQueue = AddHourCounter();
+            HC_InServer1 = AddHourCounter();
+            HC_InServer2 = AddHourCounter();
+            HC_InSystem = AddHourCounter();
+            Schedule(()=>Arrive(new Ship { Index=0}));
+           
+        }
+        void Arrive(Ship ship)
+        {
+            NoInQueue++;
+            HC_InQueue.ObserveChange(1);
+            HC_InSystem.ObserveChange(1);
+          //  HC_InQueue.ObserveCount(NoInQueue);
+            if (NoInServer1 < Config.ServerCapacity)  Schedule(()=> StartServer1(ship));
+           else if(NoInServer2 < Config.ServerCapacity) Schedule(() => StartServer2(ship));
+            else
+                PendingList.Add(ship);
+
+            Schedule(() => Arrive(new Ship { Index = ship.Index + 1 }), TimeSpan.FromDays(MathNet.Numerics.Distributions.Exponential.Sample(DefaultRS, Config.ExpectedArrivalTime)));
+            ship.TimeStamp_Arrive = ClockTime;
+            
+            Console.WriteLine($"{ClockTime} \tArrive#{ship.Index}\tQ={NoInQueue}\tS1={NoInServer1}\tS2={NoInServer2}");
+        }
+        void StartServer1(Ship ship)
+        {
+            NoInServer1++;
+            NoInQueue--;
+         
+            HC_InQueue.ObserveChange(-1);
+            HC_InServer1.ObserveChange(1);
+            //HC_InQueue.ObserveCount(NoInQueue);
+            ship.TimeStamp_Start = ClockTime;
+           
+            Schedule(() => End1(ship), TimeSpan.FromDays(MathNet.Numerics.Distributions.ContinuousUniform.Sample(DefaultRS, Config.ExpectedServiceStartTime, Config.ExpectedServiceEndTime)));
+
+            Console.WriteLine($"{ClockTime} \tStart1#{ship.Index}\tQ={NoInQueue}\tS1={NoInServer1}\tS2={NoInServer2}");
+        }
+        void StartServer2(Ship ship)
+        {
+            NoInServer2++;
+            NoInQueue--;
+            ship.TimeStamp_Start = ClockTime;
+            HC_InServer2.ObserveCount(NoInServer2);
+            HC_InQueue.ObserveChange(-1);
+            HC_InServer2.ObserveChange(1);
+          
+            Schedule(() => End2(ship), TimeSpan.FromDays(MathNet.Numerics.Distributions.ContinuousUniform.Sample(DefaultRS, Config.ExpectedServiceStartTime, Config.ExpectedServiceEndTime)));
+            Console.WriteLine($"{ClockTime} \tStart2#{ship.Index}\tQ={NoInQueue}\tS1={NoInServer1}\tS2={NoInServer2}");
+
+        }
+        void End1(Ship ship)
+        {
+            NoInServer1--;
+            HC_InSystem.ObserveChange(-1);
+            HC_InServer1.ObserveChange(-1);
+           // HC_InSystem.ObserveCount(NoInServer2);
+            if (NoInQueue > 0)
+            {
+                //because fifo
+                var next = PendingList.First();
+                PendingList.RemoveAt(0);
+                Schedule(() => StartServer1(next));
+                  }
+
+            ship.TimeStamp_End = ClockTime;
+            ProcessedList.Add(ship);
+            Console.WriteLine($"{ClockTime} \tEnd1#{ship.Index}\tQ={NoInQueue}\tS1={NoInServer1}\tS2={NoInServer2}");
+
+        }
+        void End2(Ship ship)
+        {
+            NoInServer2--;
+            HC_InServer2.ObserveChange(-1);
+            HC_InSystem.ObserveChange(-1);
+            if (NoInQueue > 0)
+            {
+                //because fifo
+                var next = PendingList.First();
+                PendingList.RemoveAt(0);
+                Schedule(() => StartServer2(next));
+            }
+
+            ship.TimeStamp_End = ClockTime;
+            ProcessedList.Add(ship);
+            Console.WriteLine($"{ClockTime} \tEnd2#{ship.Index}\tQ={NoInQueue}\tS1={NoInServer1}\tS2={NoInServer2}");
+
+        }
+    }
+}
---
+++ A1/Assignment1a/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Assignment1a
+{
+    public class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Hello World!");
+            var config = new PortStats
+            {
+                ServerCapacity = 1,
+                ExpectedArrivalTime = 1.25,
+                ExpectedServiceStartTime = 0.5,
+                ExpectedServiceEndTime = 1.5
+
+            };
+            var sim = new Port(config,seed: 0);
+            // sim.Run(TimeSpan.FromDays(100));
+
+            for(int i=0;i<1000;i++)
+                sim.Run(TimeSpan.FromDays(100));
+            Console.WriteLine($"Average cycle Time : {sim.AverageCycleTime} days");
+            Console.WriteLine($"Max Berth Time : {sim.MaxBerthTime} days");
+            Console.WriteLine($"Min Berth Time : {sim.MinBerthTime} days");
+            Console.WriteLine($"Average waiting Time : {sim.AverageWaitingTime}  days");
+            //rcq.ResourceHourCounterActive[res].AverageCount / rcq.ResourceHourCounterDynamicCapacity[res].AverageCount
+           
+            Console.WriteLine($"Average Utility of Crane1 : {sim.HC_InServer1.TotalIncrement/(100*1000)} ");
+            Console.WriteLine($"Average Utility of Crane2 : {sim.HC_InServer2.TotalIncrement / (100 * 1000)} ");
+            //Console.WriteLine($"Average cycle Time : {sim.ProcessedList.sel } days");
+
+            Console.WriteLine($"Average time the load was inside the system : {sim.HC_InSystem.AverageDuration} ");
+            Console.WriteLine($"Percentage of ships that stay in the harbour for more than 2 days : {(sim.ProcessCounts *100)/sim.HC_InQueue.TotalIncrement} ");
+        }
+    }
+}
